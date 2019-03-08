6d7343db1dbdad322fa7b98eff7e385e17c3d502
Shane D Wilson <me@shanedwilson.com>
2019-03-08 17:25:25 -0600

    working

--- JungleOverloading/Employee.cs
+++ JungleOverloading/Employee.cs
@@ -8,6 +8,7 @@ namespace JungleOverloading
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        static Random rnd = new Random();
 
         static List<string> restaurants = new List<string>
         {
@@ -18,18 +19,47 @@ namespace JungleOverloading
             "House Of Kabob"
         };
 
-        public void eat()
-        {
-            Random rnd = new Random();
-            int randNum = rnd.Next(1, restaurants.Count);   // creates a number between 1 and the length of the list
+        //public void AddEmployee(Employee employee)
+        //{
+        //    Employees.Add(employee);
+        //}
 
-            Console.WriteLine($"{FirstName} {LastName} is eating at {restaurants[randNum]}.");
+        public int generateRandomNum()
+        {
+            // creates a number between 1 and the length of the list
+            int randNum = rnd.Next(1, restaurants.Count);
+            return randNum;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{FirstName} {LastName} is eating at {restaurants[generateRandomNum()]}.");
             Console.ReadKey();
         }
 
-        public void eat(string food)
+        public void Eat(string food)
         {
             Console.WriteLine($"{FirstName} {LastName} ate {food} at the office.");
+            Console.ReadKey();
+        }
+
+        public void Eat(List<Employee> buddies)
+        {
+            string diningPals = "";
+            for(int i = 0; i < buddies.Count; i++)
+            {
+                if (buddies[i].FirstName != FirstName)
+                {
+                    if (i != buddies.Count - 1)
+                    {
+                       diningPals += buddies[i].FirstName + ",";
+                    } else
+                    {
+                        diningPals += "and " + buddies[i].FirstName;
+                    }
+                }
+            }
+            Console.WriteLine($"{FirstName} is eating at {restaurants[generateRandomNum()]} with {diningPals}.");
             Console.ReadKey();
         }
 
