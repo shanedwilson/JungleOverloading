1ca2673303ddba9ff34843a0d0a4206070007535
Shane D Wilson <me@shanedwilson.com>
2019-03-08 15:33:26 -0600

    working

---
+++ JungleOverloading/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        static List<string> restaurants = new List<string>
+        {
+            "Jay's",
+            "Taj",
+            "Hugh Baby",
+            "El Fuego",
+            "House Of Kabob"
+        };
+
+        public void eat()
+        {
+            Random rnd = new Random();
+            int randNum = rnd.Next(1, restaurants.Count);   // creates a number between 1 and the length of the list
+
+            Console.WriteLine($"{FirstName} {LastName} is eating at {restaurants[randNum]}.");
+            Console.ReadKey();
+        }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
--- JungleOverloading/Program.cs
+++ JungleOverloading/Program.cs
@@ -6,7 +6,13 @@ namespace JungleOverloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee shane = new Employee("Shane", "Wilson");
+            Employee stacye = new Employee("Stacye", "Wilson");
+            Employee nazz = new Employee("Nazz", "Wilson");
+            Employee jamie = new Employee("Jamie", "Vosmeier");
+            Employee sgt = new Employee("Sgt.", "Pepper");
+
+            stacye.eat();
         }
     }
 }
